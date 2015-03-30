b72337165943b3541964f5b0f90889740306fa3a
Richard Treichl <r.treichl@gmail.com>
2015-03-30 23:28:24 +0200

    FIX: solved servral menu bugs

--- menu/menu.h
+++ menu/menu.h
@@ -14,7 +14,8 @@
 #define MENU_MAIN_ENTRY 5
 
 uint8_t menu_handler(uint8_t *encoder_2_button, int8_t *encoder_2_count);
-uint8_t menu_scroll(uint8_t *encoder_button, int8_t *encoder_count);
+uint8_t menu_scroll(uint8_t value);
 uint8_t menu_main(uint8_t *encoder_button, int8_t *encoder_count);
+uint8_t menu_scroll_settings(uint8_t *encoder_button, int8_t *encoder_count);
 
 #endif /* _MENU_H_ */
--- menu/src/menu.c
+++ menu/src/menu.c
@@ -33,18 +33,18 @@ uint8_t menu_display()
 {
 	if (menu_display_selector == MENU_NO_FUNC_ENTRY) {
 		if (actuall_entry->previous != 0) {
-			lcd_create_view(actuall_entry->previous->text, 1, 0, 0);
+			lcd_create_view(actuall_entry->previous->text, 1, 0, 0, 0);
 		}
 		else {
-			lcd_create_view(actuall_entry->parent->text, 0, 0, 0);
+			lcd_create_view(actuall_entry->parent->text, 0, 0, 0, 0);
 		}
-		lcd_create_view("~", 0, 1, 0);
-		lcd_create_view(actuall_entry->text, 1, 1, 0);
+		lcd_create_view("~", 0, 1, 0, 0);
+		lcd_create_view(actuall_entry->text, 1, 1, 0, 0);
 		if (actuall_entry->next != 0) {
-			lcd_create_view(actuall_entry->next->text, 1, 2, 0);
+			lcd_create_view(actuall_entry->next->text, 1, 2, 0, 0);
 		}
 	}
-	lcd_create_view(0, 0, 0, 1);
+	lcd_create_view(0, 0, 0, 0, 1);
 	return 0;
 }
 
@@ -52,22 +52,22 @@ uint8_t menu_function(uint8_t *encoder_button, int8_t *encoder_count)
 {
 	switch(actuall_entry->entry_num) {
 	case MENU_FREQ_ENTRY:
-		return menu_scroll(encoder_button, encoder_count);
+		return menu_scroll_settings(encoder_button, encoder_count);
 	case MENU_VOL_ENTRY:
-		return menu_scroll(encoder_button, encoder_count);
+		return menu_scroll_settings(encoder_button, encoder_count);
 	case MENU_CONT_ENTRY:
-		return menu_scroll(encoder_button, encoder_count);
+		return menu_scroll_settings(encoder_button, encoder_count);
 	case MENU_BRIG_ENTRY:
-		return menu_scroll(encoder_button, encoder_count);
+		return menu_scroll_settings(encoder_button, encoder_count);
 	case MENU_MAIN_ENTRY:
-		return menu_main(encoder_button, encoder_count);
+		return radio_main(encoder_button, encoder_count);
 	}
 	return 0;
 }
 
 uint8_t menu_scroll_settings(uint8_t *encoder_button, int8_t *encoder_count)
 {
-	static value = 0;
+	static uint8_t value = 0;
 	if(value == 0) {
 		value = 23; //TODO load value from flash
 	}
@@ -85,19 +85,19 @@ uint8_t menu_scroll(uint8_t value)
 	uint8_t i = 0;
 	char string[2];
 
-	lcd_create_view("- ",0,2,0);
+	lcd_create_view("-", 0, 2, 0, 0);
 	for(i = 1; i <= value / 5 && i <= 12; i++) {
-		lcd_create_view("\5", 1 + i, 2, 0);
+		lcd_create_view("\5", 1 + i, 2, 0, 0);
 	}
 	if(i <= 12) {
 		string[0] = value % 5;
-		lcd_create_view(string, 1 + i, 2, 0);
+		lcd_create_view(string, 1 + i, 2, 0, 0);
 	}
 	i++;
 	for(i = i; i <= 12; i++) {
-		lcd_create_view("\0", 1 + i, 2, 0);
+		lcd_create_view("\0", 1 + i, 2, 0, 0);
 	}
-	lcd_create_view(" +", 14, 2, 0);
+	lcd_create_view("+", 14, 2, 0, 0);
 
 	return 0;
 }
@@ -122,6 +122,7 @@ uint8_t menu_handler(uint8_t *encoder_button, int8_t *encoder_count)
 			}
 			if (actuall_entry->entry_num != 0) {
 				menu_display_selector = actuall_entry->entry_num;
+				*encoder_button = BUTTON_PRESS_FREE;
 				menu_function(0, 0);
 				return 0;
 			}
