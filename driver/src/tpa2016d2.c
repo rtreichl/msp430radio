e0888314777c701983ba8f1b828e6e166976a3f3
Richard Treichl <r.treichl@gmail.com>
2015-04-08 23:17:54 +0200

    CHG: renamed function to file name

--- driver/src/tpa2016d2.c
+++ driver/src/tpa2016d2.c
@@ -6,7 +6,7 @@
  */
 #include <driver/tpa2016d2.h>
 
-void Amplifier_init(uint8_t Start_Mode, int8_t Start_Gain)
+void tpa2016d2_init(uint8_t Start_Mode, int8_t Start_Gain)
 {
 	// Enable Amplifier by pulling SDZ high
 	AMP_SHUTDOWN_DIR |= AMP_SHUTDOWN_PIN;
@@ -17,22 +17,22 @@ void Amplifier_init(uint8_t Start_Mode, int8_t Start_Gain)
 
 
 	// Enable Amplifier with R/L speaker enabled
-	i2c_write_var(I2C_AUDIO_AMP, STOP, 2, 0x01, 0xC1);
+	i2c_write_var(I2C_TPA2016D2_ADR, STOP, 2, 0x01, 0xC1);
 
 	// Wait some time before continue with configure
 	__delay_cycles(800);
 
 	// configure exended configuration
-	Amplifier_set_equalizer_mode(Start_Mode, Start_Gain);
+	tpa2016d2_equalizer_mode(Start_Mode, Start_Gain);
 }
 
-void Amplifier_Shutdown(uint8_t Shutdown) 	//only 1 or 0 => 1 for Shutdown and 0 for Release
+void tpa2016d2_shutdown(uint8_t Shutdown) 	//only 1 or 0 => 1 for Shutdown and 0 for Release
 {
 	// Send command to amplifier
-	i2c_write_var(I2C_AUDIO_AMP, STOP, 2, 0x01, 0xC3 | Shutdown<<5);
+	i2c_write_var(I2C_TPA2016D2_ADR, STOP, 2, 0x01, 0xC3 | Shutdown<<5);
 }
 
-void Amplifier_Gain(int8_t Gain)
+void tpa2016d2_gain(int8_t Gain)
 {
 	// Check gain value for valid range
 	if(Gain > 30)
@@ -41,10 +41,10 @@ void Amplifier_Gain(int8_t Gain)
 		Gain = -28;
 
 	// Send new gain to amplifier
-	i2c_write_var(I2C_AUDIO_AMP, STOP, 2, 0x05,(Gain & 0x3F));
+	i2c_write_var(I2C_TPA2016D2_ADR, STOP, 2, 0x05,(Gain & 0x3F));
 }
 
-void Amplifier_set_equalizer_mode(uint8_t Mode, int8_t Gain)
+void tpa2016d2_equalizer_mode(uint8_t Mode, int8_t Gain)
 {
 	// Check gain value for valid range
 	if(Gain > 30)
@@ -54,27 +54,27 @@ void Amplifier_set_equalizer_mode(uint8_t Mode, int8_t Gain)
 	switch(Mode) {
 	case 0:
 		// POP
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x03, 0x08, 0x0A, (Gain & 0x3F), 0x3C, 0xC2);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x03, 0x08, 0x0A, (Gain & 0x3F), 0x3C, 0xC2);
 		break;
 	case 1:
 		// CLASSIC
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x02, 0x07, 0x0A, (Gain & 0x3F), 0x3D, 0xC1);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x02, 0x07, 0x0A, (Gain & 0x3F), 0x3D, 0xC1);
 		break;
 	case 2:
 		// JAZZ
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x06, 0x14, 0x00, (Gain & 0x3F), 0x3D, 0xC1);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x06, 0x14, 0x00, (Gain & 0x3F), 0x3D, 0xC1);
 		break;
 	case 3:
 		// RAP HIP HOP
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x01, 0x0A, 0x00, (Gain & 0x3F), 0x3C, 0xC2);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x01, 0x0A, 0x00, (Gain & 0x3F), 0x3C, 0xC2);
 		break;
 	case 4:
 		// ROCK
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x03, 0x19, 0x00, (Gain & 0x3F), 0x3D, 0xC1);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x03, 0x19, 0x00, (Gain & 0x3F), 0x3D, 0xC1);
 		break;
 	case 5:
 		// NEWS VOICE
-		i2c_write_var(I2C_AUDIO_AMP, STOP, 7, 0x02, 0x02, 0x0A, 0x00, (Gain & 0x3F), 0x3E, 0xC2);
+		i2c_write_var(I2C_TPA2016D2_ADR, STOP, 7, 0x02, 0x02, 0x0A, 0x00, (Gain & 0x3F), 0x3E, 0xC2);
 		break;
 	}
 }
--- driver/tpa2016d2.h
+++ driver/tpa2016d2.h
@@ -14,21 +14,18 @@
 #include <settings/radio_pin_mapping.h>
 #include "Timer.h"
 
-#define I2C_AUDIO_AMP			0x58
-#define AUDIO_SHUT_DOWN_PORT	P3OUT
-#define AUDIO_SHUT_DOWN_PIN		BIT2
-#define AUDIO_SHUT_DOWN_DDR		P3DIR
+#define I2C_TPA2016D2_ADR		0x58
 
 enum EQUALIZER {POP = 0, CLASSIC, JAZZ, RAP_HIP_HOP, ROCK, NEWS_VOICE};
 
 enum AMPLIFIER {RELEASE = 0, SHUTDOWN};
 
-void Amplifier_init(uint8_t Start_Mode, int8_t Start_Gain);
+void tpa2016d2_init(uint8_t Start_Mode, int8_t Start_Gain);
 
-void Amplifier_Shutdown(uint8_t Shutdown);
+void tpa2016d2_shutdown(uint8_t Shutdown);
 
-void Amplifier_Gain(int8_t Gain);
+void tpa2016d2_gain(int8_t Gain);
 
-void Amplifier_set_equalizer_mode(uint8_t Mode, int8_t Gain);
+void tpa2016d2_equalizer_mode(uint8_t Mode, int8_t Gain);
 
 #endif /* VERSTAERKER_H_ */
