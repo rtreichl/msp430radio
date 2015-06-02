ac5f7e979024a41c3b701fb4ad65d34fadace7b9
Richard Treichl <r.treichl@gmail.com>
2015-06-02 19:47:00 +0200

    ADD: added a driver header file which includes all

---
+++ msp430/driver/driver.h
@@ -0,0 +1,24 @@
+/*
+ * driver.h
+ *
+ *  Created on: 26.05.2015
+ *      Author: Richard
+ */
+
+#ifndef DRIVER_DRIVER_H_
+#define DRIVER_DRIVER_H_
+
+#include <driver/encoder.h>
+#include <driver/external_interrupthandler.h>
+#include <driver/flash.h>
+#include <driver/i2c.h>
+#include <driver/lcd.h>
+#include <driver/opt3001.h>
+#include <driver/pca9530.h>
+#include <driver/pca9534.h>
+//#include <driver/pca9632.h>
+#include <driver/si4735.h>
+#include <driver/timer.h>
+#include <driver/tpa2016d2.h>
+
+#endif /* DRIVER_DRIVER_H_ */
