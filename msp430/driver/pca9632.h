31fda5c185322a4e3c36b36c5754fea646fe5367
Richard Treichl <r.treichl@gmail.com>
2015-06-18 19:15:34 +0200

    CHG: continued implementation for pca9632 driver

--- msp430/driver/pca9632.h
+++ msp430/driver/pca9632.h
@@ -12,12 +12,19 @@
 
 #define PCA9632_I2C_ADR		0x62
 
-enum PCA9632_AI_MODE {
-	PCA9632_NO_AUTO_INCREMENT = 0,
-	PCA9632_ALL_AUTO_INCREMENT = 4,
-	PCA9632_CRTL_AUTO_INCREMENT = 5,
-	PCA9632_INV_CTRL_AUTO_INCREMENT = 7
-};
+#define INVERT 1
+#define NON_INVERT 0
+
+#define OUT_DRV 1
+#define NON_OUTDRV 0
+
+#define GROUP_BLINK 1
+#define GROUP_DIM 0
+
+#define CHG_AT_STOP 0
+#define CHG_AT_ACK 1
+
+#define OUT_NE 1
 
 enum PCA9632_REGISTERS {
 	PCA9632_MODE1 = 0,
@@ -42,34 +49,68 @@ enum PCA9632_LEDOUT_STATE {
 	PCA9632_LED_GRPPWM
 };
 
-/*typedef struct pca9632_mode1 {
-	uint8_t AI:3;
-	uint8_t SLEEP:1;
-	uint8_t SUB1:1;
-	uint8_t SUB2:1;
-	uint8_t SUB3:1;
+enum PCA9632_AI_MODE {
+	PCA9632_NO_AUTO_INCREMENT = 0,
+	PCA9632_ALL_AUTO_INCREMENT = 4,
+	PCA9632_CRTL_AUTO_INCREMENT = 5,
+	PCA9632_INV_CTRL_AUTO_INCREMENT = 7
+};
+
+
+typedef struct pca9632_mode1 {
 	uint8_t ALLCALL:1;
-} PCA9632_MODE1;
+	uint8_t SUB3:1;
+	uint8_t SUB2:1;
+	uint8_t SUB1:1;
+	uint8_t SLEEP:1;
+	uint8_t AI:3;
+} PCA9632_MODE1_STC;
+
+typedef struct pca9632_register {
+	uint8_t D:4;
+	uint8_t :1;
+	uint8_t AI:3;
+} PCA9632_REGISTER_STC;
 
 typedef struct pca9632_mode2 {
-	uint8_t :2;
-	uint8_t DMBLNK0:1;
-	uint8_t INVRT:1;
-	uint8_t OCH:1;
-	uint8_t OUTDRV:1;
 	uint8_t OUTNE:2;
-} PCA9632_MODE2;
+	uint8_t OUTDRV:1;
+	uint8_t OCH:1;
+	uint8_t INVRT:1;
+	uint8_t DMBLNK0:1;
+	uint8_t :2;
+} PCA9632_MODE2_STC;
 
 typedef struct pca9632_ledout {
-	uint8_t LDR3:2;
-	uint8_t LDR2:2;
-	uint8_t LDR1:2;
 	uint8_t LDR0:2;
-} PCA9632_LEDOUT;
+	uint8_t LDR1:2;
+	uint8_t LDR2:2;
+	uint8_t LDR3:2;
+} PCA9632_LEDOUT_STC;
 
 typedef struct pca9632_subadr {
-	uint8_t ADR:7;
 	uint8_t :1;
-} PCA9632_SUBADR;*/
+	uint8_t ADR:7;
+} PCA9632_SUBADR_STC;
 
+typedef struct pca9632 {
+	PCA9632_REGISTER_STC reg;
+	PCA9632_MODE1_STC mode_1;
+	PCA9632_MODE2_STC mode_2;
+	uint8_t pwm_0;
+	uint8_t pwm_1;
+	uint8_t pwm_2;
+	uint8_t pwm_3;
+	uint8_t grp_pwm;
+	uint8_t grp_freq;
+	PCA9632_LEDOUT_STC led_out;
+	PCA9632_SUBADR_STC sub_adr_1;
+	PCA9632_SUBADR_STC sub_adr_2;
+	PCA9632_SUBADR_STC sub_adr_3;
+	PCA9632_SUBADR_STC all_call_adr;
+} PCA9632;
+
+uint8_t pca9632_init();
+uint8_t pca9632_set_register(enum PCA9632_REGISTERS cmd, void *data);
+uint8_t pca9632_get_register(enum PCA9632_REGISTERS cmd, void *data);
 #endif /* DRIVER_PCA9632_H_ */
--- msp430/driver/src/pca9632.c
+++ msp430/driver/src/pca9632.c
@@ -5,4 +5,31 @@
  *      Author: Richard
  */
 
+#include <driver/driver.h>
 
+uint8_t pca9632_init(PCA9632 *config)
+{
+	PCA_ON_OFF_SEL &= (~PCA_ON_OFF_PIN);
+	PCA_ON_OFF_DIR |= PCA_ON_OFF_PIN;
+	PCA_ON_OFF_OUT |= PCA_ON_OFF_PIN;
+
+	i2c_write_arr(PCA9632_I2C_ADR, STOP, sizeof(PCA9632), config);
+	//i2c_write_var(PCA9632_I2C_ADR, STOP, 10, 0x80, 0x81, 0x15, 0x7F, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x02);
+	//i2c_write_arr(PCA9632_I2C_ADR, STOP, sizeof(mode1), &mode1);
+	return 0;
+}
+
+uint8_t pca9632_set_register(enum PCA9632_REGISTERS cmd, void *data)
+{
+	i2c_write_var(PCA9632_I2C_ADR, STOP, 2, cmd, *((uint8_t *)data));
+
+	return 0;
+}
+
+uint8_t pca9632_get_register(enum PCA9632_REGISTERS cmd, void *data)
+{
+	i2c_write_arr(PCA9632_I2C_ADR, REPT, sizeof(cmd), cmd);
+	i2c_read(PCA9632_I2C_ADR, STOP, 1, data);
+
+	return 0;
+}
